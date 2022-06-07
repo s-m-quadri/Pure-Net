7c378a5d158a18eb42b09abd93c525313eff3c66
s-m-quadri <quadrisyedminnatullah@gmail.com>
2022-06-07 05:59:24 +0530

    attemp to scrap

--- PureNet_Live.js
+++ PureNet_Live.js
@@ -1,9 +1,8 @@
-
-function loading(){
+function loading() {
   document.getElementById("main-placeholder").innerHTML = "<h1> Loading ... </h1>";
 }
 
-function fetch_home(){
+function fetch_home() {
   loading();
 
   // Update icon bar selection
@@ -13,13 +12,21 @@ function fetch_home(){
   document.getElementById("ic-settings").className = "";
   document.getElementById("ic-devtool").className = "";
 
-  var page = document.getElementById("home-template-1").innerHTML;
-  document.getElementById("main-placeholder").innerHTML = page;
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("main-placeholder").innerHTML =
+        this.responseText;
+    }
+  };
+  request.open("GET", "home.html", true);
+  request.send();
+
   document.getElementById("body").className = "leaf_background";
   //document.getElementsByTagName("body").className = "leaf_background";
 }
 
-function fetch_search(){
+function fetch_search(input_url, input_url_domain='https://www.google.com') {
   loading();
 
   // Update icon bar selection
@@ -30,31 +37,51 @@ function fetch_search(){
   document.getElementById("ic-devtool").className = "";
   document.getElementById("body").className = "";
 
-  // Get the value from search bar
+  // var request = new XMLHttpRequest();
+  // request.onreadystatechange = function () {
+  //   if (this.readyState == 4 && this.status == 200) {
+  //     document.getElementById("main-placeholder").innerHTML =
+  //       this.responseText;
+  //   }
+  // };
+  // request.open("GET", "google_search.html", true);
+  // request.send();
   var search_text = document.getElementById("search-text").value;
-  // Grab from template and put to main section
-  var page = document.getElementById("search-template-1").innerHTML;
-  document.getElementById("main-placeholder").innerHTML = page;
-  // Update(if any?) with search bar text
-  document.getElementById("google-search").src = "https://www.google.com/search?igu=1&q=" + search_text;
+  // document.getElementById("google-search").src = "https://www.google.com/search?igu=1&q=" + search_text;
+
+  $.ajax({
+    url: './scraper-server/scraper.php',
+    type: "GET",
+    data: { URL: `https://www.google.com/search?q=${search_text}`,
+            URL_DOMAIN:"https://www.google.com"
+          }
+  }).done(function (html_file) {
+    // html_file = jQuery.parseHTML(html_file);
+    // console.log(html_file);
+    // html_file = $(html_file).filter("a");
+    // document.getElementById("main-placeholder").innerHTML=html_file
+    //html_file.replace('/(<[^>]+) style=".*?"/i', ' ');
+    // html_file_v1 = jQuery.parseHTML(html_file);
+    $("#main-placeholder").html(html_file);
+  });
 
 }
 
-function fetch_filter(action){
+function fetch_filter(action) {
   if (Boolean(action) == true)
     document.getElementById("filters-fullscreen-bar").style.height = "100%";
   else
     document.getElementById("filters-fullscreen-bar").style.height = "0%";
 }
 
-function fetch_settings(action){
+function fetch_settings(action) {
   if (Boolean(action) == true)
     document.getElementById("settings-fullscreen-bar").style.height = "100%";
   else
     document.getElementById("settings-fullscreen-bar").style.height = "0%";
 }
 
-function fetch_devtool(){
+function fetch_devtool() {
   loading();
 
   // Update icon bar selection
@@ -66,7 +93,19 @@ function fetch_devtool(){
   document.getElementById("body").className = "";
 
   // Grab from template and put to main section
-  var page = document.getElementById("devtool-template-1").innerHTML;
-  document.getElementById("main-placeholder").innerHTML = page;
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("main-placeholder").innerHTML =
+        this.responseText;
+    }
+  };
+  request.open("GET", "dev-tools.html", true);
+  request.send();
+
+  // var page = document.getElementById("devtool-template-1").innerHTML;
+  // document.getElementById("main-placeholder").innerHTML = page;
 
 }
+
+
